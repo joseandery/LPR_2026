77a011aad2d23e636c64bd93f4b5a622a15ca3df
joseandery <joseandery2201@gmail.com>
2026-06-16 08:52:19 -0300

    Aula08

--- Aula06/Notebook_Aula06/obj/Debug/net8.0/Notebook_Aula06.AssemblyInfo.cs
+++ Aula06/Notebook_Aula06/obj/Debug/net8.0/Notebook_Aula06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notebook_Aula06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90aeec32d134c7fb8b74517a990e29b8416b644c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa2cef39d540187bbbbb0ac1d8aa18e9a5e3c48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notebook_Aula06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notebook_Aula06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Aula08/notebook_aula08/Program.cs
@@ -0,0 +1,71 @@
+﻿// AULA 08 - STRUCTS
+
+using System;
+
+struct passagem
+{
+    public string origem;
+    public string destino;
+    public double valor;
+    public string comprador;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // criando uma instância da struct
+        passagem p1;
+
+        // atribuindo valores aos campos da struct
+        p1.origem = "São Paulo";
+        p1.destino = "Santa Rita";
+        p1.valor = 150.00;
+        p1.comprador = "João Silva";
+
+        // exibindo os valores dos campos da struct
+        Console.WriteLine("Passagem 1:");
+        Console.WriteLine("Origem: " + p1.origem);
+        Console.WriteLine("Destino: " + p1.destino);
+        Console.WriteLine("Valor: " + p1.valor);
+        Console.WriteLine("Comprador: " + p1.comprador);
+
+        // criando outra instância da struct em um vetor
+
+        passagem[] bilhetes_vendidos = new passagem[5];
+        bilhetes_vendidos[0]=p1;
+
+        // vetor[indice(struct)].campo
+        bilhetes_vendidos[1].origem = "Rio de Janeiro";
+        bilhetes_vendidos[1].destino = "Salvador";
+        bilhetes_vendidos[1].valor = 200.00;
+        bilhetes_vendidos[1].comprador = "Maria Souza";
+
+        bilhetes_vendidos[2].origem = "Belo Horizonte";
+        bilhetes_vendidos[2].destino = "Fortaleza";
+        bilhetes_vendidos[2].valor = 180.00;
+        bilhetes_vendidos[2].comprador = "Carlos Pereira";
+
+        // exibindo os valores dos campos da struct no vetor
+        Console.WriteLine("\nPassagem 2:");
+        Console.WriteLine("Origem: " + bilhetes_vendidos[1].origem);
+        Console.WriteLine("Destino: " + bilhetes_vendidos[1].destino
+        );
+       
+        // exibir todas as passagens do vetor bilhetes vendidos
+
+        Console.WriteLine("\nPassagens vendidas:");
+        foreach (var passagem in bilhetes_vendidos)
+        {
+            if (passagem.origem != null) // verificar se a passagem foi preenchida
+            {
+                Console.WriteLine("Origem: " + passagem.origem);
+                Console.WriteLine("Destino: " + passagem.destino);
+                Console.WriteLine("Valor: " + passagem.valor);
+                Console.WriteLine("Comprador: " + passagem.comprador);
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
